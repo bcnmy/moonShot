bff702ea63a8436676b8f061a8f4d24f02ed446b
Divya Nailwal <divyanailwal@Divyas-MacBook-Air.local>
2019-12-31 17:31:12 +0530

    price unit size

--- src/components/WaitRightComponent.js
+++ src/components/WaitRightComponent.js
@@ -16,14 +16,14 @@ class WaitRightComponent extends Component{
             <section className="wait-page-right">
                 <div className="players">
                     <div className="player-list">
-                        <div id="player-up-bet" className="player-header">Bet Up (In USDT)</div>
+                        <div id="player-up-bet" className="player-header">Bet Up <div className="price-unit">(USDT)</div></div>
                         <div className="play-list" id="play-list">
                             {upList}
                         </div>
 
                     </div>
                     <div className="player-list">
-                        <div id="player-down-bet" className="player-header">Bet Down (In USDT)</div>
+                        <div id="player-down-bet" className="player-header">Bet Down <div className="price-unit">(USDT)</div></div>
                         <div className="play-list" id="play-list">
                             {downList}
                         </div>
