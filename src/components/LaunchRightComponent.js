aef616c4d1231c6e4fb67defd04880f79ab3e796
Sachin Tomar <sachin.arox@gmail.com>
2019-12-31 19:08:15 +0530

    launch page changes

--- src/components/LaunchRightComponent.js
+++ src/components/LaunchRightComponent.js
@@ -18,12 +18,25 @@ class LaunchRightComponent extends Component{
             <div className="launch-right-section">
                 <div className="rules-heading">Game Rules</div>
                 <div className="launch-page-content">
-                    <ul>
-                        <li>Game is divided into three session </li><br/>
-                        <li>15 sec spot price of matic is selected from the binance	</li><br/>
-                        <li>Next 30 secs you can predict the price of matic whether it will go up or down </li><br/>
-                        <li>Wohoa!!, if your prediction comes true, you become a trading champ</li>
-                    </ul>
+
+                    <div id="rule-content-container">
+                        <div className="rule-item">
+                            <span className="rule-number">1</span>
+                            <span className="rule-text">15 sec Matic price is selected</span>
+                        </div>
+                        <div className="rule-item">
+                            <span className="rule-number">2</span>
+                            <span className="rule-text">Predict whether price will go UP or DOWN</span>
+                        </div>
+                        <div className="rule-item">
+                            <span className="rule-number">3</span>
+                            <span className="rule-text">Matic average price is again calculated</span>
+                        </div>
+                        <div className="rule-item">
+                            <span className="rule-number">4</span>
+                            <span className="rule-text">Wohoa!!, If your prediction comes true, you become a trading champ</span>
+                        </div>
+                    </div>
                 </div>
             </div>
 
