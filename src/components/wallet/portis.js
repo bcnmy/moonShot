e4f18387295d17784a57db008e448d6fc54b4457
Sachin Tomar <sachin.arox@gmail.com>
2020-02-04 04:06:59 +0530

    beta mainnet changes

--- src/components/wallet/portis.js
+++ src/components/wallet/portis.js
@@ -1,5 +1,8 @@
 const Portis = require('@portis/web3');
-const portis = new Portis('bedf09f0-224f-45a9-a54e-f1629c9d9592', 'maticTestnet');
+const portis = new Portis('bedf09f0-224f-45a9-a54e-f1629c9d9592', {
+        nodeUrl: 'https://betav2.matic.network',
+        chainId: 16110
+    });
 
 const portisWallet = {
     getProvider : () => {
